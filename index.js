2bee569d8acb5dc127e0137d0a8593fda9972bab
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-11-22 17:13:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+      if (distance>42){
+        return distance-42;
+      }
+      else{
+        return 42-distance
+      }
+    
+}
+console.log(distanceFromHqInBlocks(50));
+
+function distanceFromHqInFeet(distance){
+    if (distance>42){
+        return (distance-42)*264;
+      }
+      else{
+        return (42-distance)*264
+      }
+   
+}
+console.log(distanceFromHqInFeet(50))
+
+function distanceTravelledInFeet(start, destination){
+    if (destination>42){
+            return (destination-start)*264;
+          }
+          else{
+            return (start-destination)*264;
+          }
+
+}
+console.log(distanceTravelledInFeet(48,43));
+
+function calculatesFarePrice(start, destination){
+    const totalFeetTravelled = distanceTravelledInFeet(start, destination);
+    if(totalFeetTravelled <=400){
+        return 0;
+    }
+    else if(totalFeetTravelled >400 && totalFeetTravelled <= 2000){
+        return (totalFeetTravelled-400)*0.02;
+    }
+    else if(totalFeetTravelled > 2000 && totalFeetTravelled <= 2500){
+            return 25;
+        }
+    else {
+        return "cannot travel that far"
+    }        
+}
+
+
+
